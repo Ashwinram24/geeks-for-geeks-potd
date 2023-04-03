69b7f50584e0bf74a7368e4d1cac847c207d670a
Ashwinram24 <129721860+Ashwinram24@users.noreply.github.com>
2023-04-03 11:59:07 +0530

    Update Remainder on dividing by 11.cpp

--- Remainder on dividing by 11.cpp
+++ Remainder on dividing by 11.cpp
@@ -12,33 +12,25 @@ Input: x = 231456786543567898765
 Output: 1
 Explanation: 231456786543567898765 % 11 = 1
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+SOLUTION:
 */
+
+class Solution
+{
+public:
+    int xmod11(string x)
+    {
+       long long sum=0,rem=0; 
+       for(int i=0;i<x.size();i++) 
+       {
+           if((i+1)%2!=0) 
+           rem-=x[i]-'0'; 
+           else 
+           rem+=x[i]-'0'; 
+       } 
+       rem=(rem)%11; 
+      return (rem<0)?(rem+11): rem;
+       
+    
+    }
+};
