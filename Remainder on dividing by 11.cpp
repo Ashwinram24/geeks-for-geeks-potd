116742ad302fd49c0268c283c2074c8849ba0d50
Ashwinram24 <129721860+Ashwinram24@users.noreply.github.com>
2023-04-03 11:57:00 +0530

    Create Remainder on dividing by 11.cpp

---
+++ Remainder on dividing by 11.cpp
@@ -0,0 +1,44 @@
+/*
+Given a big positive number x represented as string, find value of x % 11 or x mod 11. Output is expected as an integer.
+
+Example 1:
+
+Input: x = 1345
+Output: 3
+Explanation: 1345 % 11 = 3 
+Example 1:
+
+Input: x = 231456786543567898765
+Output: 1
+Explanation: 231456786543567898765 % 11 = 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
